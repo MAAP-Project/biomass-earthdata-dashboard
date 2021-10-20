24ef76d88cbe9f63529cb8032f03353844f4253b
Phil Varner <philvarner@gmail.com>
2021-10-20 12:49:55 -0400

    remove extra all/global entry

--- app/assets/scripts/components/products/hub/index.js
+++ app/assets/scripts/components/products/hub/index.js
@@ -79,17 +79,6 @@ class ProductHub extends React.Component {
                   dashColor={metadata.color}
                 />
                 <EntriesList>
-                  <li>
-                    <EntryNavLink
-                      to='/products/global'
-                      title='Explore global'
-                    >
-                      <EntryNavLinkTitle>All</EntryNavLinkTitle>
-                      <EntryNavLinkMedia>
-                        <img src={`${baseUrl}/assets/graphics/content/products/global.jpg`} width='960' height='480' alt='Area thumbnail' />
-                      </EntryNavLinkMedia>
-                    </EntryNavLink>
-                  </li>
                   {products &&
                     products.map((item) => (
                       <li key={item.id}>
