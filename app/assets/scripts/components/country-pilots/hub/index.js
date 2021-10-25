e2ac8b755c8cdb7e208e121d16ec03fec3bd5ab0
Aimee Barciauskas <aimee@developmentseed.org>
2021-10-25 16:50:44 -0400

    Update country pilot content

--- app/assets/scripts/components/country-pilots/hub/index.js
+++ app/assets/scripts/components/country-pilots/hub/index.js
@@ -61,13 +61,19 @@ class CountryPilotHub extends React.Component {
                     factors. The biomass harmonization effort aims to demonstrate, in selected countries 
                     as case studies, the practical implementation of the refined 2019 IPCC Good Practice 
                     Guidance (GPG) with a new section on the use of biomass density maps for GHG 
-                    inventories. This effort will engage countries with established national inventory 
-                    data (NFI) to compare and enhance their forest biomass estimates with best-available 
-                    satellite-based data products, and will assess the extent to which countries without 
-                    NFI data benefit from evolving remote sensing estimates of forest biomass in their 
-                    national carbon assessment.  We rely strongly on working with national representatives 
-                    to develop synergistic approaches and bridge any gaps between evolving space-based 
-                    techniques for biomass estimation and the diverse demands and practices of countries.
+                    inventories. Based on this guidance, the use of biomass maps is increasingly important, as these wall-to-wall datasets have the potential to complement plot-based biomass measurements available through national forest inventories (NFIs). The characteristics and usefulness of biomass maps produced using space-data for national GHG inventories depend on multiple factors:
+                  </p>
+                  <p>
+                    <ul>
+                      <li>The definitions for forest and biomass or AGB used to produce the map and how this definition relates to the one used in the national GHG inventory;</li>
+                      <li>The type of space-based data sources in terms of spatial resolution, temporal coverage and the degree to which the signal responds, or is sensitive to AGB;</li>
+                      <li>The method used to construct the map. Methods can range from simple interpolation of field estimates of biomass using spatial covariates to more complex modelling of AGB using field estimates, and observed space-based data signals;</li>
+                      <li>The availability and reliability of biomass-related field data, which are of better quality than the map, and are needed to produce and validate the biomass map;</li>
+                      <li>The degree to which map uncertainty is characterised and the manner in which it is used to assess systematic deviation and precision for large area estimates in support of national GHG inventories.</li>
+                    </ul>
+                  </p>
+                  <p>
+                    With these factors in mind, biomass maps can improve the stratification of field-based biomass inventories, increase data in under-sampled or inaccessible areas, and serve as an independent data source for verification purposes (provided that the field data were not used to calibrate the biomass maps used). Such efforts can only be achieved with a close partnership and collaboration between country forest and GHG experts and biomass mapping experts. That is why this effort will engage countries with established national inventory data (NFI) to compare and enhance their forest biomass estimates with best-available satellite-based data products, and will assess the extent to which countries without NFI data benefit from evolving global remote sensing estimates of forest biomass in their national carbon assessment.  We rely strongly on working with national representatives to develop synergistic approaches and bridge any gaps between evolving space-based techniques for biomass estimation and the diverse demands and practices of countries. Their expertise and local knowledge helps us to improve in return the global estimation.
                   </p>
                   <p>
                     Click below to visit a country pilot area.
